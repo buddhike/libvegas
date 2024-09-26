1f177c0604bcf24eabcf1114ca28b7ce308bd643
Buddhike de Silva <buddhiks@amazon.com>
2024-09-26 17:53:00 +1000

    Improve source tree organisation

---
+++ src/lib/filters.go
@@ -0,0 +1,47 @@
+package lib
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"sync"
+
+	"github.com/buddhike/libvegas/lib/pb"
+)
+
+// invalidMappingFilter filters the records that are incorrectly written to a
+// shard following a split or merge operation.
+type invalidMappingFilter struct {
+}
+
+func (f *invalidMappingFilter) Apply(c *shardReaderContext, r *pb.Record) bool {
+	return r.ShardID != c.shardID
+}
+
+func newInvalidMappingFilter() *invalidMappingFilter {
+	return &invalidMappingFilter{}
+}
+
+type dedupFilter struct {
+	l    *sync.Mutex
+	seen map[string]bool
+}
+
+func (s *dedupFilter) Apply(c *shardReaderContext, r *pb.UserRecord) bool {
+	s.l.Lock()
+	defer s.l.Unlock()
+
+	h := md5.Sum(r.RecordID)
+	k := hex.EncodeToString(h[:])
+	if _, ok := s.seen[k]; ok {
+		return ok
+	}
+	s.seen[k] = true
+	return false
+}
+
+func newDedupFilter() *dedupFilter {
+	return &dedupFilter{
+		l:    &sync.Mutex{},
+		seen: make(map[string]bool),
+	}
+}
---
+++ src/lib/subscription_manager.go
@@ -0,0 +1,75 @@
+package lib
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/kinesis"
+	"github.com/aws/aws-sdk-go-v2/service/kinesis/types"
+)
+
+type subscriptionManager struct {
+	streamARN   *string
+	consumerARN *string
+	kc          consumerClient
+	p           RecordProcessor
+	rfs         []recordFilter
+	urfs        []userRecordFilter
+	done        chan struct{}
+}
+
+type consumerClient interface {
+	SubscribeToShard(ctx context.Context, params *kinesis.SubscribeToShardInput, optFns ...func(*kinesis.Options)) (*kinesis.SubscribeToShardOutput, error)
+	ListShards(ctx context.Context, params *kinesis.ListShardsInput, optFns ...func(*kinesis.Options)) (*kinesis.ListShardsOutput, error)
+}
+
+func (m *subscriptionManager) Start() {
+	l, err := m.kc.ListShards(context.TODO(), &kinesis.ListShardsInput{
+		StreamARN: m.streamARN,
+	})
+	if err != nil {
+		panic(err)
+	}
+	p := make(map[string]bool)
+	for _, s := range l.Shards {
+		p[*s.ShardId] = true
+	}
+	children := make(chan []types.ChildShard)
+	for _, s := range l.Shards {
+		if (s.ParentShardId == nil && s.AdjacentParentShardId == nil) ||
+			(s.ParentShardId != nil && !p[*s.ParentShardId]) &&
+				s.AdjacentParentShardId == nil ||
+			(s.AdjacentParentShardId != nil && !p[*s.AdjacentParentShardId]) {
+			r := &shardReader{
+				consumerARN: m.consumerARN,
+				c:           &shardReaderContext{shardID: *s.ShardId},
+				kc:          m.kc,
+				p:           m.p,
+				rfs:         m.rfs,
+				urfs:        m.urfs,
+				done:        m.done,
+				children:    children,
+			}
+			go r.Start()
+		}
+	}
+	for {
+		select {
+		case ch := <-children:
+			for _, c := range ch {
+				r := &shardReader{
+					consumerARN: m.consumerARN,
+					c:           &shardReaderContext{shardID: *c.ShardId},
+					kc:          m.kc,
+					p:           m.p,
+					rfs:         m.rfs,
+					urfs:        m.urfs,
+					done:        m.done,
+					children:    children,
+				}
+				go r.Start()
+			}
+		case <-m.done:
+			return
+		}
+	}
+}
